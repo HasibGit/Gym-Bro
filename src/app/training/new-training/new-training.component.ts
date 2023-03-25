cc4f1e5d86bac098f12a779a0915f84471d2cada
Hasib Ullah <hasibullah04@gmail.com>
2023-03-25 23:31:26 +0600

    fetched exercises from firebase db

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { WORKOUTS } from '../constants/trainings.constant';
 import { Exercise } from '../interfaces/exercise.interface';
 import { TrainingService } from '../services/training.service';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
@@ -10,16 +11,25 @@ import { TrainingService } from '../services/training.service';
   styleUrls: ['./new-training.component.scss'],
 })
 export class NewTrainingComponent implements OnInit {
+  workoutsObs$: Observable<Exercise[]>;
   workouts: Exercise[];
   selectWorkoutControl: FormControl = new FormControl('', Validators.required);
   chosenExercise: Exercise;
   @Output() newTrainingStarted: EventEmitter<boolean> = new EventEmitter();
 
-  constructor(private trainingService: TrainingService) {}
-
-  ngOnInit(): void {
-    this.workouts = this.trainingService.getAllExercises();
+  constructor(
+    private trainingService: TrainingService,
+    private _db: Firestore
+  ) {
+    const data = collection(_db, 'exercises');
+    this.workoutsObs$ = collectionData(data) as Observable<Exercise[]>;
+    this.workoutsObs$.pipe(take(1)).subscribe((exercises) => {
+      console.log(exercises);
+      this.workouts = exercises;
+    });
   }
+
+  ngOnInit(): void {}
 
   onStartTraining() {
     this.chosenExercise = this.workouts.find(
